87a3d5c37e7ccd0f7cf6bb78b599081a862dc3e5
Ivan Skorikov <ivan.s@filancy.com>
2020-03-29 00:39:54 +0200

    initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,70 @@
+<template>
+  <q-layout view="lHh lpR fFf">
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-btn
+          dense
+          flat
+          round
+          icon="menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img
+              src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg"
+            /> </q-avatar
+          >Title
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/page1" label="Page One" />
+        <q-route-tab to="/page2" label="Page Two" />
+        <q-route-tab to="/page3" label="Page Three" />
+      </q-tabs>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <q-list>
+        <q-item-label header class="text-grey-8">Essential Links</q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from "components/EssentialLink";
+
+export default {
+  name: "MainLayout",
+
+  components: {
+    EssentialLink
+  },
+
+  data() {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: [
+        {
+          title: "Docs",
+          caption: "quasar.dev",
+          icon: "school",
+          link: "https://quasar.dev"
+        }
+      ]
+    };
+  }
+};
+</script>
